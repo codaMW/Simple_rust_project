cf23c9a7227a5ddd9de9bb712325e1cf11b94d9b
codaMW <codamw011@gmail.com>
2025-12-11 07:34:05 +0200

    a simple student management system

---
+++ simple_student_mgt_system/target/debug/deps/simple_student_mgt_system.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Simple_rust_project\simple_student_mgt_system\target\debug\deps\simple_student_mgt_system.d: src\main.rs
+
+C:\Users\USER\Simple_rust_project\simple_student_mgt_system\target\debug\deps\simple_student_mgt_system.exe: src\main.rs
+
+src\main.rs:
---
+++ simple_student_mgt_system/target/debug/simple_student_mgt_system.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Simple_rust_project\simple_student_mgt_system\target\debug\simple_student_mgt_system.exe: C:\Users\USER\Simple_rust_project\simple_student_mgt_system\src\main.rs
