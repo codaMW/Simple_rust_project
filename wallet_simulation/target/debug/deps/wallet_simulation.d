7174ecab0f95e64cbaa9fd716704fd43936c4919
codaMW <codamw011@gmail.com>
2025-12-11 19:29:00 +0200

    structs practice with bitcoin wallet simulation

---
+++ wallet_simulation/target/debug/deps/wallet_simulation.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Simple_rust_project\wallet_simulation\target\debug\deps\wallet_simulation.d: src\main.rs
+
+C:\Users\USER\Simple_rust_project\wallet_simulation\target\debug\deps\wallet_simulation.exe: src\main.rs
+
+src\main.rs:
---
+++ wallet_simulation/target/debug/wallet_simulation.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Simple_rust_project\wallet_simulation\target\debug\wallet_simulation.exe: C:\Users\USER\Simple_rust_project\wallet_simulation\src\main.rs
