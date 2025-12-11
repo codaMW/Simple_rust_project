c36d9a709401a6c567c9d1ef125bd185bc55015c
codaMW <codamw011@gmail.com>
2025-12-11 12:02:32 +0200

    Reading a number and print whether it’s even or odd

---
+++ even_old_checker/target/debug/deps/even_old_checker.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Simple_rust_project\even_old_checker\target\debug\deps\even_old_checker.d: src\main.rs
+
+C:\Users\USER\Simple_rust_project\even_old_checker\target\debug\deps\even_old_checker.exe: src\main.rs
+
+src\main.rs:
---
+++ even_old_checker/target/debug/even_old_checker.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Simple_rust_project\even_old_checker\target\debug\even_old_checker.exe: C:\Users\USER\Simple_rust_project\even_old_checker\src\main.rs
