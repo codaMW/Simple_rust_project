26ce79a09769c00b1075fccf77742b6cec55ad77
codaMW <codamw011@gmail.com>
2025-12-11 15:49:25 +0200

    converting numerical score → letter grade

---
+++ grade_converter/target/debug/deps/grade_converter.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Simple_rust_project\grade_converter\target\debug\deps\grade_converter.d: src\main.rs
+
+C:\Users\USER\Simple_rust_project\grade_converter\target\debug\deps\grade_converter.exe: src\main.rs
+
+src\main.rs:
---
+++ grade_converter/target/debug/grade_converter.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Simple_rust_project\grade_converter\target\debug\grade_converter.exe: C:\Users\USER\Simple_rust_project\grade_converter\src\main.rs
