231c1ac05d1813e6ff228b13c7464b3d04aa6621
codaMW <codamw011@gmail.com>
2025-12-12 09:28:58 +0200

    ownership demostration in rust MOVE concept

---
+++ ownership101/target/debug/deps/ownership101.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Simple_rust_project\ownership101\target\debug\deps\ownership101.d: src\main.rs
+
+C:\Users\USER\Simple_rust_project\ownership101\target\debug\deps\ownership101.exe: src\main.rs
+
+src\main.rs:
---
+++ ownership101/target/debug/ownership101.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Simple_rust_project\ownership101\target\debug\ownership101.exe: C:\Users\USER\Simple_rust_project\ownership101\src\main.rs
