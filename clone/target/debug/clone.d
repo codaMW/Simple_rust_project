869b0d1ad15b76e6428ed2a6643b26ac291e2f4a
codaMW <codamw011@gmail.com>
2025-12-12 09:40:29 +0200

    demostrating cloning values from variable to the other

---
+++ clone/target/debug/clone.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Simple_rust_project\clone\target\debug\clone.exe: C:\Users\USER\Simple_rust_project\clone\src\main.rs
---
+++ clone/target/debug/deps/clone.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Simple_rust_project\clone\target\debug\deps\clone.d: src\main.rs
+
+C:\Users\USER\Simple_rust_project\clone\target\debug\deps\clone.exe: src\main.rs
+
+src\main.rs:
